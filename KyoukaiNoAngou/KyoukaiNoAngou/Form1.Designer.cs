6f1676cf8499503f9c67baf421596bd58b11971f
Mystes Yuji <tsigetartseht@yahoo.com>
2016-07-14 17:56:36 +0800

    Test upload
    Sent files via upload

---
+++ KyoukaiNoAngou/KyoukaiNoAngou/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace KyoukaiNoAngou
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.InputBox = new System.Windows.Forms.TextBox();
+            this.OutputBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(285, 168);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Encode";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(285, 210);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Decode";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Output:";
+            // 
+            // InputBox
+            // 
+            this.InputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InputBox.Location = new System.Drawing.Point(52, 53);
+            this.InputBox.Name = "InputBox";
+            this.InputBox.Size = new System.Drawing.Size(622, 24);
+            this.InputBox.TabIndex = 5;
+            // 
+            // OutputBox
+            // 
+            this.OutputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OutputBox.Location = new System.Drawing.Point(52, 98);
+            this.OutputBox.Name = "OutputBox";
+            this.OutputBox.Size = new System.Drawing.Size(619, 24);
+            this.OutputBox.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 297);
+            this.Controls.Add(this.OutputBox);
+            this.Controls.Add(this.InputBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Beyond the Cipher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox InputBox;
+        private System.Windows.Forms.TextBox OutputBox;
+    }
+}
+
---
+++ KyoukaiNoAngou/KyoukaiNoAngou/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KyoukaiNoAngou
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] bax = new byte[50];
+            int ctr = 0;
+
+            bool firstword = true;
+
+            string input = InputBox.Text;
+
+            string[] words = input.Split(' ');
+
+            OutputBox.Text = "";
+
+            foreach (string word in words)
+            { 
+                byte[] asciiBytes = Encoding.ASCII.GetBytes(word);
+
+                ctr = 0;
+
+                foreach (byte box in asciiBytes)
+                {
+                    string trf = box.ToString();
+                    int handler = int.Parse(trf);
+
+                    if(handler > 96 && handler < 120)
+                    {
+                        handler = handler + 3;
+                    }
+                    else
+                    {
+                        handler = handler - 23;
+                    }
+
+                    trf = handler.ToString();
+
+                    bax[ctr] = Byte.Parse(trf);
+                    ctr += 1;
+                }
+
+                string handler2 = Encoding.ASCII.GetString(bax);
+
+                for (int i = 0; i < bax.Length; i++)
+                {
+                    bax[i] = 0;
+                }
+
+                if (firstword == false)
+                    OutputBox.Text += " ";
+
+                OutputBox.Text += handler2;
+
+                firstword = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] bax = new byte[50];
+            int ctr = 0;
+
+            bool firstword = true;
+
+            string input = InputBox.Text;
+
+            string[] words = input.Split(' ');
+
+            OutputBox.Text = "";
+
+            foreach (string word in words)
+            {
+                byte[] asciiBytes = Encoding.ASCII.GetBytes(word);
+
+                ctr = 0;
+
+                foreach (byte box in asciiBytes)
+                {
+                    string trf = box.ToString();
+                    int handler = int.Parse(trf);
+
+                    if (handler > 99)
+                    {
+                        handler = handler - 3;
+                    }
+                    else
+                    {
+                        handler = handler + 23;
+                    }
+
+                    trf = handler.ToString();
+
+                    bax[ctr] = Byte.Parse(trf);
+                    ctr += 1;
+                }
+
+                string handler2 = Encoding.ASCII.GetString(bax);
+
+                for (int i = 0; i < bax.Length; i++)
+                {
+                    bax[i] = 0;
+                }
+
+                if(firstword == false)
+                    OutputBox.Text += " ";
+
+                OutputBox.Text += handler2;
+
+                firstword = false;
+            }
+        }
+    }
+}
